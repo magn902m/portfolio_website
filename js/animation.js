70263cf071584b28feb334a8d0692f1fc99f3ffd
magn902m <magn902m@stud.kea.dk>
2021-06-13 23:56:32 +0200

    Skift animation ved tryk på en knap

--- js/animation.js
+++ js/animation.js
@@ -7,9 +7,11 @@ function sidenVises() {
 
   const arrowTop = document.querySelector(".arrow_top");
   const logo = document.querySelector(".logo");
+  const aniKnap = document.querySelector("#ani_knap");
 
   arrowTop.addEventListener("mousedown", arrowTopHandler);
   logo.addEventListener("mouseover", logoHandler);
+  aniKnap.addEventListener("click", aniKnapHandler);
 }
 
 function arrowTopHandler() {
@@ -33,4 +35,27 @@ function logoHandler() {
   logo.classList.remove("glow");
   logo.offsetLeft;
   logo.classList.add("glow");
+}
+
+function aniKnapHandler() {
+  console.log("aniKnapHandler");
+
+  const aniTestContains = document
+    .querySelector("#ani_test")
+    .classList.contains("hop");
+  const aniTest = document.querySelector("#ani_test");
+
+  if (aniTestContains == true) {
+    // console.log("rot");
+
+    aniTest.classList.remove("hop");
+    aniTest.offsetLeft;
+    aniTest.classList.add("rotation2");
+  } else {
+    // console.log("hop");
+
+    aniTest.classList.remove("rotation2");
+    aniTest.offsetLeft;
+    aniTest.classList.add("hop");
+  }
 }
